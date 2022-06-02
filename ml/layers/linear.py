9718f605ac1ba81fd6f8b28f283c4ac69f31cd93
Himansu Didwania <hdidwania1997@gmail.com>
2022-06-02 23:52:53 +0530

    add: backward for linear layer

--- ml/layers/linear.py
+++ ml/layers/linear.py
@@ -14,5 +14,11 @@ class Linear(LearnableLayer):
         self.params["b"] = np.random.randn(1, out_dim)
 
     def forward(self, x):
-        self.buffer["x"]
+        self.buffer["x"] = x
         return np.dot(x, self.params["w"]) + self.params["b"]
+
+    def backward(self, dout):
+        self.grad["w"] = np.dot(self.buffer["x"].transpose(), dout)
+        self.grad["b"] = np.sum(dout, axis=0, keepdims=True)
+        din = np.dot(dout, self.params["w"].transpose())
+        return din
