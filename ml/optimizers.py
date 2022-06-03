54be3b67f32c85ac19718f8015e54825482ea0bd
Himansu Didwania <hdidwania1997@gmail.com>
2022-06-03 23:20:11 +0530

    add: SGD optimizer

---
+++ ml/optimizers.py
@@ -0,0 +1,13 @@
+class BaseOptimizer:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+class SGD(BaseOptimizer):
+    def __init__(self, lr):
+        self.lr = lr
+
+    def update(self, w, dw):
+        return w - self.lr * dw
